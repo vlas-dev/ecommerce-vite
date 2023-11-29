e4dcf16f0d156eecc332584aaf922bcf6d3873f8
vlas237 <83783001+vlas237@users.noreply.github.com>
2023-11-29 16:19:43 -0300

    cards with + -

--- src/components/home/HomeCards.jsx
+++ src/components/home/HomeCards.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { FaPercentage, FaGift, FaTag, FaStar } from 'react-icons/fa'
-import products from '../../services/productsData.json'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import products from '../../services/productsData.json';
 
 export default function HomeCards() {
+  // State to track quantity of each product
+  const [quantities, setQuantities] = useState(products.reduce((acc, product) => {
+    acc[product.id] = 0;
+    return acc;
+  }, {}));
+
+  // Function to increment quantity
+  const incrementQuantity = (id) => {
+    setQuantities({
+      ...quantities,
+      [id]: quantities[id] + 1
+    });
+  };
+
+  // Function to decrement quantity
+  const decrementQuantity = (id) => {
+    if (quantities[id] > 0) {
+      setQuantities({
+        ...quantities,
+        [id]: quantities[id] - 1
+      });
+    }
+  };
+
   return (
     <div className="md:pt-10 scale-90 md:scale-100">
       <h2 className="text-[2.3rem] font-[600] text-center mb-6">
@@ -11,24 +34,19 @@ export default function HomeCards() {
       </h2>
       <div className="grid grid-cols-2 md:grid-cols-4 gap-5 mx-auto md:pb-20 max-w-[1200px]">
         {products.map((product) => (
-          <Link to={product.url} key={product.id} className="w-full">
-          {/* contenedor */}
-            <div className="bg-white rounded-[6px] shadow-md hover:shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1  w-[284px] max-w-[284px] h-[469px] max-h-[469px]z">
-              {/*imagen */}
-              {/* <div className="flex justify-center  w-64 h-72"></div> */}
+          
+            <div className="relative bg-white rounded-[6px] shadow-md hover:shadow-lg transition duration-300 ease-in-out transform w-[284px] max-w-[284px] h-[469px] max-h-[469px]">
               <div className="w-[250px] h-[286px] mx-auto flex items-center">
+              <Link to={product.url} key={product.id} className="w-full">
                 <img
                   src={product.image}
-                  alt={product.title}https://theflixer.tv/watch-tv/watch-house-full-39423.4882459
+                  alt={product.title}
                   className=" bg-white w-auto h-auto object-cover"
                 />
+                </Link>
               </div>
-
               <hr className="bg-gray-300" />
-
-              {/* contenedor info */}
               <div className="p-4">
-                {/*Más vendido / oferta del día*/}
                 {product['mas-vendidos'] ? (
                   <span className="bg-[#ff7733] text-white rounded-sm px-[5px] py-[1px] uppercase text-[.75rem] font-[600] ">
                     Más vendido
@@ -36,26 +54,17 @@ export default function HomeCards() {
                 ) : (
                   ''
                 )}
-
-                {/* titulo */}
                 <h3 className="font-[400] text- text-[.95rem] text-[rgba(0,0,0,.8)]">
                   {product.titulo}
                 </h3>
-
-                {/* contenedor precio */}
                 <div>
-                  {/*precio antiguo*/}
-
-                  {product.descuento ? (
-                    <p class="line-through text-[rgba(0,0,0,.55)] text-[12px]">
+                  {product.descuento.descuento ? (
+                    <p className="line-through text-[rgba(0,0,0,.55)] text-[12px]">
                       ${product.descuento.antiguo}
                     </p>
                   ) : (
                     ''
                   )}
-
-                  {/*Precio actual y descuento*/}
-
                   <div className="flex">
                     <p className="text-2xl">${product.precio}</p>
                     {product.descuento.descuento ? (
@@ -67,17 +76,28 @@ export default function HomeCards() {
                     )}
                   </div>
                 </div>
-
-                {/*Tipo de envío*/}
                 <p className="text-[#00a650] font-[600] text-[14px]">
                   {product.envioGratis ? 'Envío gratis' : 'Sin envío incluido'}
                 </p>
-                
               </div>
+              
+              {/* Quantity Controls - Positioned at bottom right */}
+              <div className="absolute bottom-2 right-2 flex items-center">
+                <button onClick={() => decrementQuantity(product.id)} className="px-2 py-1 border rounded-l">
+                  -
+                </button>
+                <span className="px-2 py-1 border-t border-b">
+                  {quantities[product.id]}
+                </span>
+                <button onClick={() => incrementQuantity(product.id)} className="px-2 py-1 border rounded-r">
+                  +
+                </button>
+              </div>
+
             </div>
-          </Link>
+          
         ))}
       </div>
     </div>
-  )
+  );
 }
