28c55f5921c7583d688ed5fab7c2123e65c22f4b
vlas-dev <83783001+vlas-dev@users.noreply.github.com>
2023-12-13 19:05:35 -0300

    unchange vite.config

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,28 +1,39 @@
-import React from "react";
-import { loadStripe } from "@stripe/stripe-js";
+import React, { useEffect, useState } from "react";
 import { Elements } from "@stripe/react-stripe-js";
 import CheckoutForm from "../components/checkout/CheckoutForm";
 import StripeElements from "../components/checkout/StripeElements";
 import CheckoutInfo from "../components/checkout/CheckoutInfo";
 
 export const Checkout = ({ options }) => {
-  const stripePromise = loadStripe(
-    "pk_test_51OLTCEH1kbewROnHlhKKtvH9pYc2jholZqwoMB7gr68ao8f7RVFnaMPlgvBR2g3wlzN4qyvBTk9hYIlJ1FZOTAoi00b2wvNDoO"
-  );
+  const [stripe, setStripe] = useState(null);
+
+  useEffect(() => {
+    // Dynamically import the Stripe module
+    import("@stripe/stripe-js")
+      .then((stripeJs) => stripeJs.loadStripe("pk_test_51OLTCEH1kbewROnHlhKKtvH9pYc2jholZqwoMB7gr68ao8f7RVFnaMPlgvBR2g3wlzN4qyvBTk9hYIlJ1FZOTAoi00b2wvNDoO"))
+      .then((stripeInstance) => {
+        setStripe(stripeInstance);
+      })
+      .catch((error) => {
+        console.error("Error loading Stripe:", error);
+      });
+  }, []);
+
+  if (!stripe) {
+    return <div>Loading Stripe...</div>;
+  }
 
   return (
     <div className="pt-32">
-      <Elements stripe={stripePromise} options={options}>
+      <Elements stripe={stripe} options={options}>
         <div className="flex justify-center space-x-8">
           <div>
             <CheckoutForm />
           </div>
-
           <div>
             <CheckoutInfo />
           </div>
         </div>
-
         <div className="mt-8">
           <StripeElements />
         </div>
