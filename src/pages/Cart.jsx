a4e650ef691dbbc25d47fff5cdc4a6a5a25fe75b
vlas-dev <83783001+vlas-dev@users.noreply.github.com>
2023-12-13 21:45:25 -0300

    success page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,87 +1,71 @@
 import React, { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { RiDeleteBin7Line, RiDeleteBin7Fill } from "react-icons/ri";
-import { CartContext } from "../components/context/CartContext"; // Update the import path as per your project structure
+import { CartContext } from "../components/context/CartContext";
 import { useEffect } from "react";
 import crudAxios from "../config/axios";
 
-export default function CartPage({setOption}) {
+export default function CartPage({ setOption }) {
   const { cartItems, removeFromCart, increaseQuantity, decreaseQuantity } =
-      useContext(CartContext);
+    useContext(CartContext);
 
-    
   const calculateTotalPrice = () => {
-    return cartItems.reduce(
-      (total, item) => total + item.precio * item.quantity,
-      0
-    );
+    return cartItems.reduce((total, item) => total + item.precio * item.quantity, 0);
   };
-    const totalPrice =calculateTotalPrice()
-    const navigate = useNavigate()
-    const [precio, setPrecio] = useState(0)
-    const [amount, setAmount] = useState({
-      amount:0
-    })
-    const  turnFloatIntoInt = (numero) => {
-      let cadena = numero.toString();
-      let sinPunto = cadena.replace(".", "");
-      let entero = parseInt(sinPunto);
-      return parseInt(entero/10);
-    }
+  const totalPrice = calculateTotalPrice();
+  const navigate = useNavigate();
+  const [precio, setPrecio] = useState(0);
+  const [amount, setAmount] = useState({
+    amount: 0,
+  });
 
- 
-    
-  const toCheckOut = () =>{
-    console.log(amount)
+  const turnFloatIntoInt = (numero) => {
+    let cadena = numero.toString();
+    let sinPunto = cadena.replace(".", "");
+    let entero = parseInt(sinPunto);
+    return parseInt(entero / 10);
+  };
+
+  const toCheckOut = () => {
+    console.log(amount);
 
     const consultarApi = async () => {
       try {
-          const token = localStorage.getItem("x-token");
-          const config = {
-            headers: { "x-token": token },
-          };
-          const res = await crudAxios.post(`/payment/process`,amount,config);
-          const {client_secret} = await res.data
-          setOption({  
-           
-          clientSecret:client_secret})
-          
-        } catch (error) {
-          console.log(error);
-
-        }
-      };
-      consultarApi();
-      
-
-    // navigate('/checkout')
-  }
-  useEffect(()=>{
-    const amount ={
-      amount:turnFloatIntoInt(precio)
+        const token = localStorage.getItem("x-token");
+        const config = {
+          headers: { "x-token": token },
+        };
+        const res = await crudAxios.post(`/payment/process`, amount, config);
+        const { client_secret } = await res.data;
+        setOption({
+          clientSecret: client_secret,
+        });
+      } catch (error) {
+        console.log(error);
       }
-    setAmount(amount)
-  },[precio])
-  
-  useEffect(()=>{
-    setPrecio(totalPrice)
-  },[totalPrice])
+    };
+    consultarApi();
+  };
 
+  useEffect(() => {
+    const amount = {
+      amount: turnFloatIntoInt(precio),
+    };
+    setAmount(amount);
+  }, [precio]);
 
-  const totalItems = cartItems.reduce(
-    (total, item) => total + item.quantity,
-    0
-  );
+  useEffect(() => {
+    setPrecio(totalPrice);
+  }, [totalPrice]);
+
+  const totalItems = cartItems.reduce((total, item) => total + item.quantity, 0);
   const [hoveredItemId, setHoveredItemId] = useState(null);
 
   if (cartItems.length === 0) {
     return (
       <div className="container mx-auto pt-32 text-center">
         <h2 className="text-2xl font-bold">Carrito vacío</h2>
-        <Link
-          to="/"
-          className="text-indigo-600 mt-3 inline-block font-semibold"
-        >
+        <Link to="/" className="text-indigo-600 mt-3 inline-block font-semibold">
           Volver
         </Link>
       </div>
@@ -89,27 +73,24 @@ export default function CartPage({setOption}) {
   }
 
   return (
-    <div className="container mx-auto pt-36">
+    <div className="container mx-auto p-10 md:p-20">
       <div className="flex flex-col md:flex-row shadow-md">
-        <div className="w-full  bg-white px-10 py-10 rounded">
+        <div className="w-full bg-white px-10 py-10 rounded">
           <div className="flex justify-between border-b pb-8">
             <h1 className="font-semibold text-2xl">Carrito</h1>
-            <h2 className="font-semibold text-2xl">
-              {totalItems} Productos
-            </h2>{" "}
-            
+            <h2 className="font-semibold text-2xl">{totalItems} Productos</h2>
           </div>
-          <div className="mt-10 mb-5">
+          <div className="mt-4 mb-5 ">
             {cartItems.map((item) => (
               <div
                 key={item.id}
-                className="flex justify-between items-center border-b mb-5 pb-5"
+                className="flex flex-col md:flex-row justify-between items-center border-b mb-5 pb-5"
               >
-                <div className="flex items-center">
+                <div className="flex items-center mb-3 md:mb-0 ">
                   <img
                     src={`/images/products/${item.titulo}.png`}
                     alt={item.titulo}
-                    className="h-20 w-20 object-cover rounded mr-4"
+                    className="h-16 w-16 md:h-20 md:w-20 object-cover rounded mr-4 "
                   />
                   <div>
                     <p className="font-bold">{item.titulo}</p>
@@ -118,14 +99,14 @@ export default function CartPage({setOption}) {
                 </div>
                 <div className="flex items-center font-semibold text-xl">
                   <button
-                    className="bg-gray-200 text-indigo-600 px-3 py-1 rounded-md hover:bg-gray-300 ml-1  min-w-[36px]"
+                    className="bg-gray-200 text-indigo-600 px-2 sm:px-3 py-1 rounded-md hover:bg-gray-300 ml-1 sm:ml-2 min-w-[30px] sm:min-w-[36px]"
                     onClick={() => decreaseQuantity(item.id)}
                   >
                     -
                   </button>
-                  <span className="mx-3">{item.quantity}</span>
+                  <span className="mx-2 sm:mx-3">{item.quantity}</span>
                   <button
-                    className="bg-indigo-600 text-white px-3 py-1 rounded-md hover:bg-indigo-700"
+                    className="bg-indigo-600 text-white px-2 sm:px-3 py-1 rounded-md hover:bg-indigo-700"
                     onClick={() => increaseQuantity(item.id)}
                   >
                     +
@@ -146,26 +127,27 @@ export default function CartPage({setOption}) {
               </div>
             ))}
           </div>
-        
         </div>
 
-        <div className="w-full px-8 py-10">
-          <h1 className="font-semibold text-2xl border-b pb-8">Mi Orden</h1>
-          <div className="flex md:flex-row flex-col justify-between mt-10 mb-5">
+        <div className="w-full md:w-1/2 px-4 sm:px-8 py-10">
+          <h1 className="font-semibold text-2xl border-b pb-4 sm:pb-8">Mi Orden</h1>
+          <div className="flex md:flex-row flex-col justify-between mt-4 sm:mt-10 mb-4 sm:mb-5">
             <span className="font-semibold uppercase mb-4">Total </span>
-            <span className="font-semibold ">
-              ${totalPrice.toFixed(2)}
-            </span>
+            <span className="font-semibold ">${totalPrice.toFixed(2)}</span>
           </div>
           <div>
- 
-              <button onClick={toCheckOut} className="bg-indigo-600 hover:bg-indigo-700 px-5 py-2 text-sm text-white w-full rounded font-bold">
-                Completar Orden
-              </button>
- 
+            <button
+              onClick={toCheckOut}
+              className="bg-indigo-600 hover:bg-indigo-700 px-4 sm:px-5 py-2 text-sm text-white w-full rounded font-bold"
+            >
+              Completar Orden
+            </button>
           </div>
         </div>
       </div>
+      <Link to="/" className="text-indigo-600 inline-block font-semibold mt-4">
+        Continuar comprando
+      </Link>
     </div>
   );
 }
--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -1,18 +1,35 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { CartContext } from '../components/context/CartContext';
 
-export default function Success() {
-
-
+const Success = () => {
   const { clearCart } = useContext(CartContext);
 
   useEffect(() => {
-
     clearCart();
-  }, []);
-
+  }, [clearCart]);
 
   return (
-    <div className='pt-56 flex justify-center'>Success!</div>
-  )
-}
+    <div className="flex flex-col items-center justify-center p-8 min-h-screen w-full bg-white">
+      <div className="w-full max-w-2xl p-6 bg-white shadow-md rounded-lg">
+        
+        <h2 className="text-green-600 text-3xl font-semibold mb-4 text-center mb-8">
+          ¡Gracias por tu compra!
+        </h2>
+        <p className="text-gray-800 text-lg mb-8 text-center">
+          Tu pago ha sido procesado con éxito.
+        </p>
+        <div className="text-center mt-6">
+          <Link
+            to="/"
+            className="inline-block bg-indigo-600 text-white px-6 py-3 rounded-md hover:bg-indigo-700 text-lg font-semibold transition duration-300"
+          >
+            Continuar comprando
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Success;
